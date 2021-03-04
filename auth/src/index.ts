7f20bfcba3a4e0ea013e18a7a8f137bcb2a3d5de
mwinel <mwinel@live.com>
2021-03-04 14:39:14 +0300

    Test auth and hospitals deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ import { app } from "./app";
 // Connect to a MongoDB instance.
 // Listen to port 3000.
 const start = async () => {
-  console.log('Starting...')
+  console.log('Starting.....')
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined.");
   }
