2e95f205c743aef1bf6c8f76f24bf58202d9bc0a
mwinel <mwinel@live.com>
2021-03-04 11:16:32 +0300

    Test depl manifests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -23,7 +23,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log("Auth service listening on port 3000.:");
+    console.log("Auth service listening on port 3000.");
   });
 };
 
